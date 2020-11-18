89ac577b4e0b553fad1a6d70937a6b5c23ced2fe
Roger <rogersampaio@gmail.com>
2020-11-18 01:14:00 +0100

    Music volume menu working

--- Assets/Script/MusicClass.cs
+++ Assets/Script/MusicClass.cs
@@ -1,24 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MusicClass : MonoBehaviour
 {
-    private AudioSource _audioSource;
-     private void Awake()
-     {
-         DontDestroyOnLoad(transform.gameObject);
-         _audioSource = GetComponent<AudioSource>();
-     }
- 
-     public void PlayMusic()
-     {
-         if (_audioSource.isPlaying) return;
-         _audioSource.Play();
-     }
- 
-     public void StopMusic()
-     {
-         _audioSource.Stop();
-     }
+    private static MusicClass instance = null;
+    public static MusicClass Instance { get { return instance; } }
+
+    private float musicVolume = 1f;
+
+    public AudioSource _audioSource;
+
+    public Slider sliderVolume;
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            return;
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            Play();
+        }
+    }
+
+    private void Start(){
+        if (instance != null)
+        {
+            sliderVolume.value = instance.musicVolume;
+        }
+    }
+
+    public void Play()
+    {
+        _audioSource.Play();
+    }
+    void Update()
+    {
+        _audioSource.volume = musicVolume;
+    }
+
+    public static void SetVolume(float vol)
+    {
+        if (instance != null)
+        {
+            instance.musicVolume = vol;
+            return;
+        }
+    }
 }
