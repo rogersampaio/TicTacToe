4de05446a4db7a649be50400b18497b845f13134
Roger <rogersampaio@gmail.com>
2020-12-30 15:01:03 +0100

    Aded camera FOV auto orientation adjustment

---
+++ Assets/Script/CameraOrientation.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+
+public class CameraOrientation : MonoBehaviour
+{
+    public GameObject cameraObject;
+    private Camera cameraInstance;
+    private ScreenOrientation currentScreenOrientation;
+
+    void Start()
+    {
+        cameraInstance = cameraObject.GetComponent<Camera>();
+        currentScreenOrientation = Screen.orientation;
+        AdjustCameraFOV();
+    }
+
+    void Update()
+    {
+        if (Screen.orientation != currentScreenOrientation)
+            AdjustCameraFOV();
+    }
+
+    private void AdjustCameraFOV()
+    {
+        currentScreenOrientation = Screen.orientation;
+        if (currentScreenOrientation == ScreenOrientation.Landscape || currentScreenOrientation == ScreenOrientation.LandscapeLeft || currentScreenOrientation == ScreenOrientation.LandscapeRight)
+            cameraInstance.fieldOfView = 42;
+        if (currentScreenOrientation == ScreenOrientation.Portrait || currentScreenOrientation == ScreenOrientation.PortraitUpsideDown)
+            cameraInstance.fieldOfView = 70;
+    }
+}
